1c29744ba231f6b6980184768d139fc8aa41c563
Alexandre Pereira <alexandre.gl.pereira@outlook.pt>
2025-09-27 21:34:26 +0000

    Altered terraform docs

--- 2-Terraform/3-log-analytics/la.tf
+++ 2-Terraform/3-log-analytics/la.tf
@@ -1,6 +1,6 @@
 resource "azurerm_log_analytics_workspace" "Log_Analytics_WorkSpace" {
   # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-  name                = var.log_analytics_workspace_name
+  name                = "law-${var.name}"
   location            = var.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
   sku                 = var.sku
